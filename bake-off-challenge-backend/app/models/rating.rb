c206964bce27e4c023fc8f2d77d77d12bade6d12
kev-kev <kevin.clark93@gmail.com>
2020-04-07 15:00:12 -0400

    deliverables done, will work on the bonuses later today

--- bake-off-challenge-backend/app/models/rating.rb
+++ bake-off-challenge-backend/app/models/rating.rb
@@ -2,6 +2,7 @@ class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :bake
 
+  validates :score, presence: true
   validates :score, inclusion: { in: "0".."10" }
   validates :user, uniqueness: { scope: :bake }
 
